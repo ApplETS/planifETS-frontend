0651604ab7ec353512288c5165b8831024ffc227
Mohamed Idrissi <mhdhafdi2@gmail.com>
2025-02-15 20:20:15 -0500

    remove useless comments

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,19 +36,13 @@ export default function LoginPage() {
 
   return (
     <div className="mx-auto w-full max-w-md rounded-[27px] border-2 border-textDarkBackground bg-yearSection p-6 shadow-md md:p-8">
-      {/* Header Text */}
       <h1 className="mb-2 text-2xl font-medium text-textDarkBackground underline">
         Se connecter avec Signets
       </h1>
-
-      {/* Subtext */}
       <p className="mb-4 text-base text-textDarkBackground">
         Accédez à tous vos cours complétés!
       </p>
-
-      {/* Login Form */}
       <form className="space-y-4" onSubmit={handleSubmit}>
-        {/* Code Universel */}
         <InputField
           id="codeUniversel"
           label="Code universel"
@@ -57,8 +51,6 @@ export default function LoginPage() {
           placeholder="AS12345"
           onChange={e => setCodeUniversel(e.target.value)}
         />
-
-        {/* Mot de Passe */}
         <InputField
           id="motDePasse"
           label="Mot de passe"
@@ -67,11 +59,9 @@ export default function LoginPage() {
           placeholder="Votre mot de passe"
           onChange={e => setMotDePasse(e.target.value)}
         />
-
         {/* Error Message */}
         {/* errorMessage && <div className="mb-4 text-sm text-red-500">{errorMessage}</div> */}
 
-        {/* Login Button */}
         <button
           type="submit"
           className="w-full rounded-md bg-buttonTags py-3 font-bold uppercase text-background transition-colors duration-200 hover:bg-loginButtonHover"
@@ -79,8 +69,6 @@ export default function LoginPage() {
           SE CONNECTER
         </button>
       </form>
-
-      {/* Separator */}
       <div className="my-4 flex items-center">
         {/* FIXME: hardcoded color */}
         <hr className="grow border-t border-[#D5C8EE]" />
@@ -88,8 +76,6 @@ export default function LoginPage() {
         {/* FIXME: hardcoded color */}
         <hr className="grow border-t border-[#D5C8EE]" />
       </div>
-
-      {/* Guest Option */}
       <Link href="/planner">
         <button
           type="button"

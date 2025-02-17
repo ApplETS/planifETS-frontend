0f0bebf78397c245fc5c642fb9debade438db51b
Mohamed Idrissi <mhdhafdi2@gmail.com>
2025-02-17 00:56:09 -0500

    test

--- src/components/Planner/SessionHeader.tsx
+++ src/components/Planner/SessionHeader.tsx
@@ -15,7 +15,7 @@ type SessionHeaderProps = {
 
 const SessionHeader: FC<SessionHeaderProps> = ({
   sessionName,
-  sessionYear: year,
+  sessionYear,
   totalCredits,
   isNoAvailabilityData,
 }) => {
@@ -28,7 +28,7 @@ const SessionHeader: FC<SessionHeaderProps> = ({
         <h3 className="text-lg font-bold">
           {sessionName}
           {' '}
-          {year}
+          {sessionYear}
         </h3>
         {isNoAvailabilityData && (
           <Tooltip
@@ -49,7 +49,7 @@ const SessionHeader: FC<SessionHeaderProps> = ({
       <div className="flex w-auto justify-end sm:mt-0">
         <CreditsBadge
           credits={totalCredits}
-          testId={`session-${sessionName}-${year}-credits`}
+          testId={`session-${sessionName}-${sessionYear}-credits`}
         />
       </div>
     </div>
